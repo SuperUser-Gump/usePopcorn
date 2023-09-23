e6459abf471ee0302188b03d7568bbd9c59098b0
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-23 17:01:45 +0700

    chore: Switch from isolated environment to current environment
    - Removed the StarRatings component from main.jsx.
    - Added App.js and index.css back to main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRatings from "./StarRatings.jsx";
-// import App from './App.jsx'
-// import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StarRatings
-      maxRating={"5"}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRatings size={24} color="red" defaultRating={3} />
-    {/*<App />*/}
+    <App />
   </React.StrictMode>
 );
