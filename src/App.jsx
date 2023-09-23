e81919c83808a1a319f2561b006dd6e444b360a8
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-23 19:01:51 +0700

    feat: Consume OMDb API
    - Updated the .gitignore file to exclude the .env file.
    - Added a fetch request to retrieve movies with Interstellar in their title.
    - Rendered the fetch result in the MovieList component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,9 +50,17 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = import.meta.env.VITE_KEY;
+
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
 
   return (
     <>
