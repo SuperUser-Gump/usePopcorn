83947ca4bfc5264a4534e9cab057d801e1f7d21f
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-24 20:53:08 +0700

    feat: Add effect to dynamically change page title to selected movie title
    - Added an effect to dynamically update the page title to the title of the selected movie.

--- src/App.jsx
+++ src/App.jsx
@@ -322,6 +322,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     },
     [selectedId]
   );
+
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
