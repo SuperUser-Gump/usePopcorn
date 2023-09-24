45dc7c676b93aad3a6028fee6a144d878225b77e
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-24 20:58:46 +0700

    feat: Add cleanup function to reset dynamically changing page title effect
    - Added a cleanup function to ensure that the dynamically changing page title effect is reset properly.

--- src/App.jsx
+++ src/App.jsx
@@ -327,6 +327,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
     },
     [title]
   );
