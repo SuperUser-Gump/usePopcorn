49cb98198dcc5c19f1d48d97d99b2a3c017c6a84
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-24 21:10:57 +0700

    feat: Add cleanup function to fetch operation to prevent race conditions
    - Added a cleanup function for the fetch operation to prevent potential race conditions and ensure reliable data handling.

--- src/App.jsx
+++ src/App.jsx
@@ -79,13 +79,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) throw new Error("Something went wrong");
@@ -94,9 +96,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          console.error(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+            console.error(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -109,6 +114,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
