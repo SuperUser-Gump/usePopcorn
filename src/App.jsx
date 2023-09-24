77c9660ff7593237d3cbab4277641ad7f4406d3d
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-24 17:40:19 +0700

    feat: Display movie details in MovieDetails component
    - Added a movie state to MovieDetails to store movie details received from the API.
    - Displayed movie details, including title, year, poster, runtime, IMDB rating, plot, released year, actors, director, and genre, in the MovieDetails.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRatings from "./StarRatings.jsx";
 
 const tempMovieData = [
   {
@@ -119,11 +120,14 @@ export default function App() {
           {error && <ErrorMessage message={error} />}
         </Box>
         {selectedId ? (
-          <Box header={null}>
-            <MovieDetails
-              selectedId={selectedId}
-              onCloseMovie={handleCloseMovie}
-            />{" "}
+          <Box
+            header={
+              <button className="btn btn-back" onClick={handleCloseMovie}>
+                &larr;
+              </button>
+            }
+          >
+            <MovieDetails selectedId={selectedId} />
           </Box>
         ) : (
           <Box header={<WatchedSummary watched={watched} />}>
@@ -249,12 +253,72 @@ function Movie({ movie, onSelectMovie }) {
   );
 }
 
-function MovieDetails({ selectedId, onCloseMovie }) {
+function MovieDetails({ selectedId }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+        setIsLoading(false);
+      }
+
+      getMovieDetails();
+    },
+    [selectedId]
+  );
   return (
     <div className="details">
-      <button className="btn btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <img src={poster} alt={`Poster of ${title}`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMDB Rating
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              <StarRatings maxRating={10} size={24} />
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
