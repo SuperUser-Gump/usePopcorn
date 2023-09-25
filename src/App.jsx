33ceb3d7397e99bf4407f6524faf71a73e2d4510
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-25 17:54:04 +0700

    feat: Add local storage for watched state data
    - Added a callback function to retrieve watched data from local storage and initialize the watched state in the useState hook.
    - Added a function to persist watched data to local storage within the useEffect hook.

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,15 @@ const KEY = import.meta.env.VITE_KEY;
 
 export default function App() {
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [query, setQuery] = useState("");
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = JSON.parse(localStorage.getItem("watched"));
+    return storedValue;
+  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -29,6 +33,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
