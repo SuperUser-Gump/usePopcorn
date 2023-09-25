75694a56bfe2d87f57743ee41aab12a333923287
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-26 02:26:45 +0700

    feat: Add click count tracking with useRef() for rating decision
    - Utilized useRef() to add click count tracking, allowing the recording of how many times a user clicks on ratings before submitting an actual rating.

--- src/App.jsx
+++ src/App.jsx
@@ -256,6 +256,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current++;
+    },
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -300,6 +309,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchMovie);
     onCloseMovie();
