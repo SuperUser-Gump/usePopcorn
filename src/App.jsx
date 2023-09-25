1b2328bb7bdd7ec2aef7b8ea6c1f14793fb69618
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-26 00:26:35 +0700

    feat: Enhance Search component with autofocus
    - Added autofocus to the search input, ensuring it receives focus when rendered.
    - Added functionality to focus the search input when the Enter key is pressed.
    - Added functionality to clear the search input when the Enter key is pressed after typing a search query.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRatings from "./StarRatings.jsx";
 
 const average = (arr) =>
@@ -160,6 +160,25 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -167,6 +186,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
